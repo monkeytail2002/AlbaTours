e4a49d74ca6d79764d7c123f6b08806f66ecd7dc
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2021-01-26 14:22:14 +0000

    Add files via upload

--- Tours/Tours/urls.py
+++ Tours/Tours/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bookings.urls')),
+    path('staff/', include('staff.urls')), 
+    path('controlpanel/', include('staff.urls')),
 
 ]
 
